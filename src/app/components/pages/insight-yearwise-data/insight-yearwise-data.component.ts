c91f7c5a6a3f635218a3185a59263b7652bc6671
shailendra <shail5788@gmail.com>
2019-06-06 00:34:54 +0530

    done css and insight accordian fixes

--- src/app/components/pages/insight-yearwise-data/insight-yearwise-data.component.ts
+++ src/app/components/pages/insight-yearwise-data/insight-yearwise-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Renderer } from "@angular/core";
 import { WPAPIService } from "../../../../services/wpapi.service";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
 @Component({
   selector: "app-insight-yearwise-data",
   templateUrl: "./insight-yearwise-data.component.html",
@@ -10,6 +11,9 @@ export class InsightYearwiseDataComponent implements OnInit {
   subCategories: any;
   posts: any;
   selectedItem;
+  isfirst = false;
+  currentSubCate;
+  getApiData;
   constructor(private wpservice: WPAPIService, private render: Renderer) {}
   getSubCategoryData(category, event) {
     this.selectedItem = category;
@@ -21,12 +25,37 @@ export class InsightYearwiseDataComponent implements OnInit {
       });
     // this.render.setElementClass(event.target, "active", true);
   }
-  getMonthPost(id, event) {
-    console.log(id);
-    this.wpservice.getPostFromCategory(`?categories=${id}`).subscribe(posts => {
-      this.posts = posts;
-      console.log(this.posts);
-    });
+  getMonthPost(subcategory, event, toggle) {
+    // this code for accordian
+    if (this.currentSubCate) {
+      if (subcategory == this.currentSubCate) {
+        this.isfirst = !toggle;
+      } else {
+        this.isfirst = true;
+      }
+      this.currentSubCate = subcategory;
+    } else {
+      this.isfirst = !toggle;
+      this.currentSubCate = subcategory;
+    }
+
+    if (this.getApiData) {
+      if (this.getApiData != this.posts) {
+        this.wpservice
+          .getPostFromCategory(`?categories=${subcategory.id}`)
+          .subscribe(posts => {
+            this.posts = posts;
+            // console.log(this.posts);
+          });
+      }
+    } else {
+      this.wpservice
+        .getPostFromCategory(`?categories=${subcategory.id}`)
+        .subscribe(posts => {
+          this.posts = posts;
+          // console.log(this.posts);
+        });
+    }
   }
   ngOnInit() {
     this.wpservice.getCategory("?parent=14").subscribe(categories => {
