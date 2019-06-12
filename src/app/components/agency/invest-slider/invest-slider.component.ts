5e21bbe9e7408ce93fa70477b0fff869ffcee6ae
shailendra <shail5788@gmail.com>
2019-06-12 18:03:02 +0530

    add message popup with design

--- src/app/components/agency/invest-slider/invest-slider.component.ts
+++ src/app/components/agency/invest-slider/invest-slider.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit, Input, ViewChild, AfterViewInit } from "@angular/core";
-import { SwiperComponent } from 'angular2-useful-swiper';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  AfterViewInit
+} from "@angular/core";
+import { SwiperComponent } from "angular2-useful-swiper";
 
 @Component({
   selector: "app-invest-slider",
@@ -19,23 +25,30 @@ export class InvestSliderComponent implements AfterViewInit {
     paginationClickable: true,
     nextButton: ".swiper-button-next",
     prevButton: ".swiper-button-prev",
-    spaceBetween: 30,
-	//initialSlide: 0
+    spaceBetween: 30
+    //initialSlide: 0
   };
-  @ViewChild('usefulSwiper') public usefulSwiper: SwiperComponent;
+  @ViewChild("usefulSwiper") public usefulSwiper: SwiperComponent;
   ngAfterViewInit() {
     if (this.dataset.acf) {
       this.slideDatas = this.dataset.acf.how_do_i_invest;
     } else {
       this.slideDatas = this.dataset;
+      //console.log(this.slideDatas);
     }
   }
   ngOnChanges() {
-    this.slideDatas = this.dataset;
-	// console.log('usefulSwiper', this.usefulSwiper);
-	if(this.usefulSwiper) {
-	  this.usefulSwiper.swiper.slideTo(0);
-	  this.usefulSwiper.swiper.startAutoplay();
-	}
+    if (this.dataset.acf) {
+      this.slideDatas = this.dataset.acf.how_do_i_invest;
+    } else {
+      this.slideDatas = this.dataset;
+    }
+    // console.log("usefulSwiper", this.usefulSwiper);
+    if (typeof this.usefulSwiper != "undefined") {
+      if (this.usefulSwiper) {
+        this.usefulSwiper.swiper.slideTo(0);
+        this.usefulSwiper.swiper.startAutoplay();
+      }
+    }
   }
 }
